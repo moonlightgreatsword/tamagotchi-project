ebd45e120a5cb23544bedea5e3f2a346c18e0943
msuh <suh.mikep@gmail.com>
2022-07-05 17:47:42 -0700

    displaying pet stats

--- js/app.js
+++ js/app.js
@@ -1,7 +1,30 @@
 class Tamagotchi {
     constructor(name) {
         this.name = name;
+        this.hunger = 1;
+        this.sleepiness = 1;
+        this.boredom = 1;
+        this.age = 1;
     }
 }
 
-const pet = new Tamagotchi("name");
+const pet = new Tamagotchi("Tony");
+
+const petName = document.createElement("h3");
+petName.innerText = `Name: ${pet.name}`
+const petHunger = document.createElement("h3");
+petHunger.innerText = `Hunger: ${pet.hunger}`
+const petSleepiness = document.createElement("h3");
+petSleepiness.innerText = `Sleepiness: ${pet.sleepiness}`
+const petBoredom = document.createElement("h3");
+petBoredom.innerText = `Boredom: ${pet.boredom}`
+const petAge = document.createElement("h3");
+petAge.innerText = `Age: ${pet.age}`
+
+const stats = document.querySelector("#stats")
+
+stats.appendChild(petName);
+stats.appendChild(petHunger);
+stats.appendChild(petSleepiness);
+stats.appendChild(petBoredom);
+stats.appendChild(petAge);
